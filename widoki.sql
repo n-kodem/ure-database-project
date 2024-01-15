8ba10ed6d9c752e6dab09180b949ec85a6979908
Kacper Hnatyszyn <90875438+rerecum@users.noreply.github.com>
2024-01-15 07:31:47 +0100

    Update widoki.sql

--- widoki.sql
+++ widoki.sql
@@ -1,1 +1,13 @@
+SELECT NIP, NazwaFirmy, Adres, Telefon, Email
+FROM WykazKRS;
 
+
+SELECT NazwaFirmy, Udzialowcy
+FROM WykazKRS
+LEFT JOIN Udzialowcy ON WykazKRS.ID = Udzialowcy.FirmaID
+WHERE WykazKRS.NIP = '1234567890';
+
+
+SELECT NazwaFirmy, IloscPracownikow, SrednieZarobki
+FROM WykazKRS
+LEFT JOIN DanePracownicze ON WykazKRS.ID = DanePracownicze.FirmaID;
